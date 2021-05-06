8b4c5bafbb701267fc378f0704af7e182c4b2016
Maciej Pałach <macipal716@student.polsl.pl>
2021-05-06 22:16:22 +0200

    Add project files.

---
+++ Controllers/HelloWorld.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Text.Encodings.Web;
+
+namespace MvcCoreWebApp.Controllers
+{
+    public class HelloWorld : Controller
+    {
+        public string Index()
+        {
+            return "Default Action";
+        }
+
+        public string Welcome(string name, int numTimes = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
+            //return "Welcome Action";
+        }
+    }
+}
